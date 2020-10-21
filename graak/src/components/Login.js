394715025f4e56e1b293713d1b233e37a5648863
Ronit-Mathur <ronstergreen@gmail.com>
2020-10-21 00:05:40 -0700

    Desktop Changes

--- graak/src/components/Login.js
+++ graak/src/components/Login.js
@@ -1,25 +1,85 @@
 import React from 'react';
-import "../Styles/Login.css"
-import { Component } from 'react';
+import "../Styles/Login.css";
+import axios from 'axios';
+import { Component, useState } from 'react';
+import { API_BASE_URL, ACCESS_TOKEN_NAME } from '../Constants/apiConstants'
 
-class Login extends Component {
+function Login(props) {
+
+    const [state, setState] = useState({
+        username: "",
+        password: ""
+    })
+
+    const handleChange = (e) => {
+        const {id, value} = e.target
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
+
+    const handleSubmitClick = (e) => {
+        e.preventDefault();
+        sendDetailsToServer()
+    }
+
+    const sendDetailsToServer = () => {
+        if (state.username.length && state.password.length) {
+            // props.showError(null); // showError doesnt exist?? -- Figure out replacement
+            const payload={
+                "username":state.username,
+                "password":state.password,
+            }
+            axios.post(API_BASE_URL+"/user/login", payload)
+                .then(function (response) {
+                    if (response.status === 200) {
+                        setState(prevState => ({
+                            ...prevState,
+                            "successMessage" : 'Login successful. Redirecting..'
+                        }))
+                        redirectToLogin(); // Change for user page?
+                        props.showError(null)
+                    } else {
+                        props.showError("An Error Occured");
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        } else {
+            props.showError("Please enter a valid username and password")
+        }
+    }
+
+    // Future use - copy for registration page
+    const redirectToHome = () => {
+        props.updateTitle('LandingPage')
+        props.history.push('/LandingPage');
+    }
+
+    const redirectToLogin = () => {
+        props.updateTitle('Login')
+        props.history.push('/login'); 
+    }
+
+
     
-    render() {
     return (
         <div className="Login">
         <p>Login</p>
         <div>
         <form action="/action_page.php">
         <label>Username</label>
-        <input type="text" id="username" name="username" placeholder="Username ..." />
+        <input type="text" id="username" name="username" value={state.username} placeholder="Username ..." onChange={handleChange}/>
         <label>Password</label>
-        <input type="text" id="password" name="password" placeholder="***********" />
-        <input type="submit" value="Submit" />
+        <input type="text" id="password" name="password" value={state.password} placeholder="***********" onChange={handleChange} />
+        <input type="submit" value="Submit" onClick={handleSubmitClick}/>
         </form>
         </div>
         </div>
     );
-    }
+
 }
 
 export default Login
