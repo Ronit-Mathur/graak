0e8d71fcc019e41e157f17af4d90a0789c229902
Ronit-Mathur <ronstergreen@gmail.com>
2020-11-09 19:02:15 -0800

    Extras

--- graak/src/components/LandingPage.js
+++ graak/src/components/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./Login";
-import { Component } from 'react';
-class LandingPage extends Component {
+import { Component } from "react";
+import { Link } from "react-router-dom";
 
-render() {
-  return <Login/>;
-}
+class LandingPage extends Component {
+  // LandingPage appears at the top of every rerender ->
+  render() {
+    return <Link to="/Login">To Login</Link>;
+  }
 }
 export default LandingPage;
